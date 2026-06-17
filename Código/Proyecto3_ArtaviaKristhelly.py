9014fd6762cdee7b929929f2cf64433720745014
Kristhelly Artavia <kristhellyartavia@gmail.com>
2026-06-17 02:47:21 -0600

    Add files via upload
    Se agrego método actualizar_datos en clase Entrenador

--- Código/Proyecto3_ArtaviaKristhelly.py
+++ Código/Proyecto3_ArtaviaKristhelly.py
@@ -131,7 +131,48 @@ class Entrenador(Persona):
         return (Persona.mostrar_datos(self)
                 + f"\nLicencia: {self.licencia}"
                 + f"\nAños de experiencia como entrenador: {self.experiencia_anios}"
-                + f"\nSistema táctico preferido: {self.sistema_juego}") 
+                + f"\nSistema táctico preferido: {self.sistema_juego}")
+
+    def actualizar_datos(self, nuevoNombre = None, nuevoApellido = None, nuevaFecha_nacimiento = None, nuevaNacionalidad = None,nuevaLicencia = None,nuevaExperiencia_anios = None, nuevoSistema_juego = None):
+       if (nuevoNombre != None):
+            if not isinstance(nuevoNombre, str):
+                return "El nuevo nombre debe ser un texto"
+        if (nuevoApellido != None):   
+            if not isinstance(nuevoApellido, str):
+                return "El nuevo apellido debe ser un texto"
+        if (nuevaFecha_nacimiento != None):    
+            if not isinstance(nuevaFecha_nacimiento, str):
+                return "La nueva fecha de nacimiento debe ser un texto en formato DD/MM/AAAA"
+        if (nuevaNacionalidad != None):    
+            if not isinstance(nuevaNacionalidad, str):
+                return "La nueva nacionalidad debe ser un texto"
+        if (nuevaLicencia != None):
+            if not isinstance (nuevaLicencia, str):
+                return "Error: La nueva licencia debe ser un texto"
+        if (nuevaExperiencia_anios != None):
+            if not isinstance (nuevaExperiencia_anios, int):
+                return "Error: Los nuevos años de experiencia deben ser un número entero"
+        if(nuevoSistema_juego != None):
+            if not isinstance(nuevoSistema_juego,str):
+                return "Error: El nuevo sistema táctico preferido debe ser un texto"
+
+        if (nuevoNombre != None):
+            self.nombre = nuevoNombre
+        if (nuevoApellido != None):
+            self.apellido = nuevoApellido
+        if (nuevaFecha_nacimiento != None):
+            self.fecha_nacimiento = nuevaFecha_nacimiento
+        if (nuevaNacionalidad != None):
+            self.nacionalidad = nuevaNacionalidad
+        if (nuevaLicencia != None):
+            self.licencia = nuevaLicencia
+        if (nuevaExperiencia_anios != None):
+            self.experiencia_anios = nuevaExperiencia_anios
+        if(nuevoSistema_juego != None):
+            self.sistema_juego = nuevoSistema_juego
+        
+        
+        return "La actualización se realizó con éxito" 
 
         
 
